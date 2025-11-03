671afe3934d9da8b99abd481bc4b4aee2c0f2d73
ryhan27 <muhamadrayhanramadhansyah80@gmail.com>
2025-11-03 21:17:08 +0700

    udh nih

--- Design/BuyerForm.cs
+++ Design/BuyerForm.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SbnApplicationUTS2.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace SbnApplicationUTS.Design
 {
     public partial class BuyerForm : Form
     {
+        private SBNContext db = new SBNContext();
+        private int selectedBuyerId = -1;
         public BuyerForm()
         {
             InitializeComponent();
@@ -24,8 +31,23 @@ namespace SbnApplicationUTS.Design
 
         private void BuyerForm_Load(object sender, EventArgs e)
         {
-
+            LoadData();
         }
+        private void LoadData()
+        {
+         dgvBuyer.DataSource = db.Buyers.ToList();
+            ClearForm();
+        }
+        private void ClearForm()
+        {
+            txtCode.Clear();
+            txtName.Clear();
+            txtEmail.Clear();
+            txtPhone.Clear();
+            txtAddress.Clear();
+            selectedBuyerId = -1;
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -34,7 +56,84 @@ namespace SbnApplicationUTS.Design
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+        }
 
+        private void btnAddBuyer_Click(object sender, EventArgs e)
+        {
+            var buyer = new Buyer
+            {
+                BuyerCode = txtCode.Text,
+                BuyerName = txtName.Text,
+                Email = txtEmail.Text,
+                PhoneNumber = txtPhone.Text,
+                Address = txtAddress.Text
+            };
+
+            db.Buyers.Add(buyer);
+            db.SaveChanges();
+
+            MessageBox.Show("Data berhasil ditambahkan!");
+            LoadData();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (selectedBuyerId == -1)
+            {
+                MessageBox.Show("Pilih data yang ingin diupdate terlebih dahulu!");
+                return;
+            }
+
+            var buyer = db.Buyers.Find(selectedBuyerId);
+            if (buyer != null)
+            {
+                buyer.BuyerCode = txtCode.Text;
+                buyer.BuyerName = txtName.Text;
+                buyer.Email = txtEmail.Text;
+                buyer.PhoneNumber = txtPhone.Text;
+                buyer.Address = txtAddress.Text;
+
+                db.SaveChanges();
+                MessageBox.Show("Data berhasil diupdate!");
+                LoadData();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedBuyerId == -1)
+            {
+                MessageBox.Show("Pilih data yang ingin dihapus terlebih dahulu!");
+                return;
+            }
+
+            var buyer = db.Buyers.Find(selectedBuyerId);
+            if (buyer != null)
+            {
+                db.Buyers.Remove(buyer);
+                db.SaveChanges();
+                MessageBox.Show("Data berhasil dihapus!");
+                LoadData();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void dgvBuyer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var row = dgvBuyer.Rows[e.RowIndex];
+                selectedBuyerId = Convert.ToInt32(row.Cells["BuyerId"].Value);
+                txtCode.Text = row.Cells["BuyerCode"].Value.ToString();
+                txtName.Text = row.Cells["BuyerName"].Value.ToString();
+                txtEmail.Text = row.Cells["Email"].Value.ToString();
+                txtPhone.Text = row.Cells["PhoneNumber"].Value.ToString();
+                txtAddress.Text = row.Cells["Address"].Value.ToString();
+            }
         }
     }
 }
